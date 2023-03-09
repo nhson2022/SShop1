022f3159d9c03fd036e44b6d3f3cadec4d7b7953
nhson2022 <sonpgdlh@gmail.com>
2023-03-09 15:55:54 +0700

    Update item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,8 @@ class ItemsController < ApplicationController
 
   # GET /items or /items.json
   def index
-    @items = Item.all
+    @items = Item.includes(:category).all
+    @categories = Category.all
   end
 
   # GET /items/1 or /items/1.json
